ba41cd5ea2b6014035b36de03cd006a51a8206e2
curtis <crutchc@yahoo.com>
2019-11-30 16:43:05 -0600

    fixed

---
+++ BookShelf/Models/AuthorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    public class AuthorViewModel
+    {
+        public Author Author { get; set; }
+        public List<Book> Books { get; set; }
+
+        public List<SelectListItem> BookOptions
+        {
+            get
+            {
+                return Books?.Select(b => new SelectListItem(b.Title, b.Id.ToString())).ToList();
+            }
+        }
+    }
+}
